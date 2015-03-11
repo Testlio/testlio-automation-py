46d93897240287ec2636ac2f2ba32464fe09ea96
Martin Raag <martin@testlio.com>
2015-03-11 15:52:43 +0200

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(name='testlio',
+    version='0.1',
+    description='Testlio Python test automation utilities',
+    author='Testlio',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=['selenium']
+)
