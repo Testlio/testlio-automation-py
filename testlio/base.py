44f73f6d8f224d8f644faecf93139db7239e6fcf
Marko Kruustuk <marko@testlio.com>
2015-03-29 20:03:01 +0300

    Added find_element_by_id

--- testlio/base.py
+++ testlio/base.py
@@ -147,6 +147,8 @@ class TestlioAutomationTest(unittest.TestCase):
             return self._find_element_by_name(kwargs['name'])
         elif kwargs.has_key('class_name'):
             return self._find_element_by_class_name(kwargs['class_name'])
+        elif kwargs.has_key('id'):
+            return self._find_element_by_id(kwargs['id'])
         elif kwargs.has_key('xpath'):
             return self._find_element_by_xpath(kwargs['xpath'])
         else:
@@ -171,6 +173,13 @@ class TestlioAutomationTest(unittest.TestCase):
             return self.driver.find_element_by_class_name(class_name)
         except NoSuchElementException, e:
             self.event.error(element_name=name)
+            raise e
+
+    def _find_element_by_id(self, element_id):
+        try:
+            return self.driver.find_element_by_id(element_id)
+        except NoSuchElementException, e:
+            self.event.error(id=element_id)
             raise e
 
     def _alert_is_present(self):
