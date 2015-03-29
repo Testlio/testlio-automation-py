dc2799090d4d39980be3063bc9d7103fc6084095
Marko Kruustuk <marko@testlio.com>
2015-03-29 16:47:45 +0300

    Implicit wait 30

--- testlio/base.py
+++ testlio/base.py
@@ -55,7 +55,7 @@ class TestlioAutomationTest(unittest.TestCase):
             desired_capabilities=capabilities,
             command_executor=os.getenv('EXECUTOR'))
 
-        self.driver.implicitly_wait(60)
+        self.driver.implicitly_wait(30)
 
     def teardown_method(self, method):
         # self.log({'event': {'type': 'stop'}})
