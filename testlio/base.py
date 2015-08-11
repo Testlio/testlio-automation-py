03c25b67eee7be992b3004678be68c888c3e17e8
Valdo Purde <valdo@testlio.com>
2015-08-12 01:23:16 +0300

    increased timeout to 99s

--- testlio/base.py
+++ testlio/base.py
@@ -59,7 +59,7 @@ class TestlioAutomationTest(unittest.TestCase):
             desired_capabilities=capabilities,
             command_executor=os.getenv('EXECUTOR'))
 
-        self.driver.implicitly_wait(70)
+        self.driver.implicitly_wait(99)
 
     def teardown_method(self, method):
         # self.log({'event': {'type': 'stop'}})
