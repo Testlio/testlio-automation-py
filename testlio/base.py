9afa643cb80ff71fb48a0b87857be9b7c726f4ca
Valdo Purde <valdo@testlio.com>
2015-09-02 10:24:05 +0300

    initial wait to 2 min

--- testlio/base.py
+++ testlio/base.py
@@ -59,7 +59,7 @@ class TestlioAutomationTest(unittest.TestCase):
             desired_capabilities=capabilities,
             command_executor=os.getenv('EXECUTOR'))
 
-        self.driver.implicitly_wait(99)
+        self.driver.implicitly_wait(130)
 
     def teardown_method(self, method):
         # self.log({'event': {'type': 'stop'}})
