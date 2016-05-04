548de31461bb6beab4851ba4469e67b45f6c64fd
Romero Paoliello <romerop@gmail.com>
2016-05-04 11:51:26 +0100

    Adding calabash tcp dump validator

---
+++ testlio/tcpdump.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from time import sleep
+import threading
+
+
+local = threading.local()
+
+
+def init(tcpdump_file_name='./dump.txt', ads_host='pubads.g.doubleclick.net', tcpdump_hours_behind=4):
+    local.tcpdump_file_name = tcpdump_file_name
+    local.ads_host = ads_host
+    local.tcpdump_hours_behind = tcpdump_hours_behind  # number of hours the tcp dump entries are behind the server time
+
+
+def validate(origin, offset_in_seconds=60):
+    assert local.tcpdump_file_name and local.ads_host and local.tcpdump_hours_behind, \
+        'You need to initialise the tcp dump validator before using it. For that you need to call tcpdump.init()'
+
+    datetime_now = datetime.now() - timedelta(hours=local.tcpdump_hours_behind)
+    datetime_from = datetime_now - timedelta(seconds=offset_in_seconds)
+    datetime_to = datetime_now + timedelta(seconds=offset_in_seconds)
+
+    while (datetime.now() - timedelta(hours=local.tcpdump_hours_behind)) < datetime_to:
+        tcpdump_lines = _read()
+        for line in tcpdump_lines:
+            # print str(datetime_from < line['datetime']) + ' ' + str(datetime_to > line['datetime'])
+            if datetime_from < line['datetime'] < datetime_to and origin in line['path']:
+                print('Found TCP dump entry - origin=' + origin + ', methodCalledOn=' + str(datetime_now) + ', datetime_now=' + str(datetime.now() - timedelta(hours=local.tcpdump_hours_behind)))
+                return True
+        sleep(1)  # wait for one second before reading the file again
+
+    print('TCP dump entry not found - origin=' + origin + ', methodCalledOn=' + str(datetime_now) + ", offset_in_seconds=" + str(offset_in_seconds))
+    return False
+
+
+def _read():
+    with open(local.tcpdump_file_name, 'r') as tcpdump_file:
+        file_content = []
+        for line_string in tcpdump_file:
+            parsed_line = _parse_line(line_string, [local.ads_host])
+            if parsed_line:
+                file_content.append(parsed_line)
+        return file_content
+
+
+def _parse_line(line_string, valid_hosts=None):
+    try:
+        line = line_string.split(' ')
+
+        host = line[5]
+        if valid_hosts and host not in valid_hosts:
+            return
+
+        return {
+            'datetime': datetime.strptime(line[0] + line[1], '%Y-%m-%d%H:%M:%S'),
+            'host': host,
+            'path': line[8]
+        }
+    except:
+        # print('Failed trying to parse line, skipping... [' + line_string + ']')
+        return
