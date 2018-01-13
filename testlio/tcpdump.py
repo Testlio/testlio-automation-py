4852c0136781741a6751cfe6e293a9ae4fb636e3
luisatheodoro <theodoro.luisa@gmail.com>
2018-01-13 01:14:04 +0000

    testing tcp dump

--- testlio/tcpdump.py
+++ testlio/tcpdump.py
@@ -132,9 +132,9 @@ def _read():
         file_content = []
         for line_string in tcpdump_file:
             parsed_line = _parse_line(line_string, local.host)
-            print(parsed_line)
             if parsed_line:
                 file_content.append(parsed_line)
+                print(parsed_line)
         return file_content
 
 
