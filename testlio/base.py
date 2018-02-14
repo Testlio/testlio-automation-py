af5e72a1fb6898163cdb9a47cabc9a1478ba64b3
dzaiats <denys.zaiats@gmail.com>
2018-02-14 19:21:50 +0100

    [master] - added decode/encode to batch

--- testlio/base.py
+++ testlio/base.py
@@ -599,7 +599,10 @@ class TestlioAutomationTest(unittest.TestCase):
     def verify_in_batch(self, data, case_sensitive=True, strict_visibility=True, screenshot=True, strict=False,
                         with_timeout=2):
         sleep(with_timeout)
-        page_source = self.driver.page_source.decode().encode('utf-8')
+        try:
+            page_source = self.driver.page_source.encode('utf-8')
+        except:
+            page_source = self.driver.page_source.encode('ascii', 'ignore').decode('ascii')
 
         self.event.assertion(data="*** BATCH VERIFICATION START ***", screenshot=self.screenshot())
 
